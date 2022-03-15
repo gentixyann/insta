65e995a542fbfd1d834a5a148a2144c9e0d069ba
Gento <a551.return@gmail.com>
2022-03-15 21:44:16 +0900

    signUpUser作成

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,37 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Signing Up User
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file,
+  }) async {
+    String res = "Some error Occurred";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty ||
+          file != null) {
+        // registering user in auth with email and password
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}

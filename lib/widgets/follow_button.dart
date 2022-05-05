0b2337fd99a3757cae1b9d3512bea8507664bbcf
Gento <a551.return@gmail.com>
2022-05-05 11:01:03 +0900

    iphone se 3 でA RenderFlex overflowed by 3.0 pixels on the right.を解消

--- lib/widgets/follow_button.dart
+++ lib/widgets/follow_button.dart
@@ -18,7 +18,7 @@ class FollowButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 2),
+      padding: const EdgeInsets.all(2),
       child: TextButton(
         onPressed: function,
         child: Container(
@@ -37,7 +37,7 @@ class FollowButton extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          width: 250,
+          width: 200,
           height: 27,
         ),
       ),
